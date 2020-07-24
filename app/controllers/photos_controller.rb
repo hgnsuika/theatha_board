4f136d1385e1e39010ecf4ee82b8d02f9012a418
shinichiroh nishijima <hgnsuika@gmail.com>
2020-07-24 05:56:30 +0000

    add CRUD

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,21 +5,55 @@ class PhotosController < ApplicationController
   end
 
   def show
+      @photo = Photo.find(params[:id])
   end
 
   def new
+      @photo = Photo.new
   end
 
   def create
+      @photo = Photo.new(photo_params)
+
+    if @photo.save
+      flash[:success] = 'Photo が正常に投稿されました'
+      redirect_to @photo
+    else
+      flash.now[:danger] = 'Photo が投稿されませんでした'
+      render :new
+    end
   end
 
   def edit
+      @photo = Photo.find(params[:id])
   end
 
   def update
+      @photo = Photo.find(params[:id])
+
+    if @photo.update(photo_params)
+      flash[:success] = 'photo は正常に更新されました'
+      redirect_to @photo
+    else
+      flash.now[:danger] = 'photo は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+
+    flash[:success] = 'photo は正常に削除されました'
+    redirect_to root_url
   end
+  
+  private
+
+  # Strong Parameter
+  def photo_params
+    params.require(:photo).permit(:caption, :embended_html)
+  end
+  
     
 end
