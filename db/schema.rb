b3553a78d43251ff32d2fa19ace58c2de1471bd5
shinichiroh nishijima <hgnsuika@gmail.com>
2020-07-25 08:36:25 +0000

    changed user-id to user references

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_110541) do
+ActiveRecord::Schema.define(version: 2020_07_25_083051) do
 
   create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "user_id"
     t.text "embended_html"
     t.string "caption"
     t.datetime "created_at", null: false
