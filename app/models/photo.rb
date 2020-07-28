e50c841b1523e087b656a1a22b3b63a170e3efc1
shinichiroh nishijima <hgnsuika@gmail.com>
2020-07-28 11:33:45 +0000

    comment

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
     belongs_to :user
     validates :embended_html, presence: true, length: { maximum: 255 }
-    validates :caption, presence: true, length: { maximum: 255 }
+    validates :caption, presence: true, length: { maximum: 50 }
     
      has_many :comments
 
