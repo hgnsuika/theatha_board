d0c0d894be9476d3e526125caf6a8babe7bf6656
shinichiroh nishijima <hgnsuika@gmail.com>
2020-07-29 16:48:37 +0000

    change top page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,6 @@ class Photo < ApplicationRecord
     validates :embended_html, presence: true, length: { maximum: 255 }
     validates :caption, presence: true, length: { maximum: 50 }
     
-     has_many :comments
-
+     has_many :comments, dependent: :destroy
 end
 
